8cdf9b3e4ca0975fc3be4b7acfa926c4ddba0dd2
karimialvin-sketch <karimi.alvin@strathmore.edu>
2026-06-08 13:03:53 +0300

    lab 2.7 js 101

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*let age=20;
+var course ="BBIT";
+const university = "Strathmore University";
+
+//let age=22
+//var course ="BCOM"
+//const university = "KCA University"
+if (true){
+  let time = "11:30"
+  var day = "Monday"
+  const year =2024
+}
+console.log(year)*/
+
+function calculateArea(height, width)
+{
+    if (height ==null) {
+        console.log('Height missing');
+        return console.log('Height missing');
+    }else if (width == null) {
+        console.log('Width missing');
+        return console.log('Width missing');
+    }else {
+    let area = height * width;
+    return area;
+  //console.log("Function calculateArea")
+  //alert("function called /executed")
